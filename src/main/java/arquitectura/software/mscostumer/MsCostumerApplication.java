9a4189a6ff01b78110adc8f7eab83cc4b62a8538
Mauricio Illanes <mauricioillanes17@gmail.com>
2021-11-04 00:56:29 -0400

    first commit

---
+++ src/main/java/arquitectura/software/mscostumer/MsCostumerApplication.java
@@ -0,0 +1,13 @@
+package arquitectura.software.mscostumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCostumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCostumerApplication.class, args);
+	}
+
+}
