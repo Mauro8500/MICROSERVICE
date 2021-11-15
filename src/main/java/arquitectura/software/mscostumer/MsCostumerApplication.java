626b11278f9d4143b44cbbf55e88be30fed2cf0b
Mauricio Illanes <mauricioillanes17@gmail.com>
2021-11-15 11:47:44 -0400

    docker

--- src/main/java/arquitectura/software/mscostumer/MsCostumerApplication.java
+++ src/main/java/arquitectura/software/mscostumer/MsCostumerApplication.java
@@ -3,9 +3,12 @@ package arquitectura.software.mscostumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+
 public class MsCostumerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/arquitectura/software/mscostumer/api/CustomerController.java
+++ src/main/java/arquitectura/software/mscostumer/api/CustomerController.java
@@ -3,8 +3,13 @@ package arquitectura.software.mscostumer.api;
 import arquitectura.software.mscostumer.config.CustomerConfig;
 import arquitectura.software.mscostumer.entity.Customer;
 import arquitectura.software.mscostumer.repository.CustomerRepository;
+import arquitectura.software.mscostumer.service.AccountService;
+import ch.qos.logback.classic.Logger;
+
 import java.util.List;
 import java.util.Optional;
+
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.CustomAutowireConfigurer;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,14 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class CustomerController{
 
+    private static Logger LOGGER = LoggerFactory.logger(CustomerController.class);
+
     @Autowired
     private CustomerRepository customerRepository;
     @Autowired
     private CustomerConfig customerConfig;
+    @Autowired
+    private AccountService accountService;
     
     @RequestMapping(path = "/test",
                     method = RequestMethod.GET)
     public String testCustomer(){
+        LOGGER.info("Probando endpoint ms-costumer");
+        String result = accountService.testAccount();
+        LOGGER.info("Probando endpoint ms-costumer", result);
         return "Ms-customer";
     }
 
